66eae41fb522f806dd3efecda341100460f558a9
Sushi10522 <sushmithan1022005@gmail.com>
2025-08-14 15:23:55 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,17 @@
 import json
 
 def lambda_handler(event, context):
-    print("Event:", event)
-
-    name = None
-    if event.get("queryStringParameters") and "name" in event["queryStringParameters"]:
-        name = event["queryStringParameters"]["name"]
     
-    message = f"Hello, {name}!" if name else "Hello from Lambda!"
+    if event.get("queryStringParameters")
+        a = event["queryStringParameters"]["a"]
+        b = event["queryStringParameters"]["b"]
+    
+    
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": json.dumps({"message": message})
+        "body": json.dumps({"message": a+b})
     }
